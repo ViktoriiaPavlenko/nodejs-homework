a56ec3e3db7b3767d9c8804451992e130167aa19
ViktoriiaPavlenko <vktr9999@ukr.net>
2022-04-14 22:05:50 +0300

    made corrections

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,7 +1,4 @@
 const { User } = require("../../models");
-// const { Unauthorized } = require("http-errors");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: null });
