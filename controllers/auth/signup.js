2a3eaa9497d3261e2505c3e38c4c16fa726ffc58
ViktoriiaPavlenko <vktr9999@ukr.net>
2022-04-20 21:31:42 +0300

    adding avatar

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,6 @@
 const { User } = require("../../models");
 const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
@@ -8,11 +9,13 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict(`User with ${email} already exist`);
   }
+  const avatarURL = gravatar.url(email);
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   const result = await User.create({
     email,
     password: hashPassword,
     subscription,
+    avatarURL,
   });
   res.status(201).json({
     status: "success",
@@ -21,6 +24,7 @@ const signup = async (req, res) => {
       user: {
         email,
         subscription,
+        avatarURL,
       },
     },
   });
